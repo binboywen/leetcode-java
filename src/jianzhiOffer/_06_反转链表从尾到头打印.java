15810204711ab9a506d5761f9852e48bb92906ec
BINPC <dream_wt_possible@163.com>
2020-07-21 17:25:14 +0800

    commit "modify files"

--- src/jianzhiOffer/_06_反转链表从尾到头打印.java
+++ src/jianzhiOffer/_06_反转链表从尾到头打印.java
@@ -6,15 +6,13 @@ import java.util.Stack;
 /**
  * 从尾到头反过来打印出每个结点的值。
  */
-public class _06_printListFromTailToHead {
+public class _06_反转链表从尾到头打印 {
     /**
      * 使用递归
      * 要逆序打印链表 1->2->3（3,2,1)，可以先逆序打印链表 2->3(3,2)，
      * 最后再打印第一个节点 1。而链表 2->3 可以看成一个新的链表，
      * 要逆序打印该链表可以继续使用求解函数，
      * 也就是在求解函数中调用自己，这就是递归函数。
-     * @param listNode
-     * @return
      */
     public static ArrayList<Integer> printListFromTailToHead(ListNode listNode){
         ArrayList<Integer> ret = new ArrayList<>();
@@ -27,16 +25,14 @@ public class _06_printListFromTailToHead {
 
     /**
      * 头插法
-     * @param listNode
-     * @return
      */
     public static ArrayList<Integer> printListFromTailToHead_2(ListNode listNode){
         ListNode head = new ListNode(-1);
         while(listNode != null){
-            ListNode temp = listNode.next;
+            ListNode next = listNode.next;
             listNode.next = head.next;
             head.next = listNode;
-            listNode = temp;
+            listNode = next;
         }
 
         ArrayList<Integer> ret = new ArrayList<>();
@@ -50,8 +46,6 @@ public class _06_printListFromTailToHead {
 
     /**
      * 使用栈
-     * @param listNode
-     * @return
      */
     public ArrayList<Integer> printListFromTailToHead_3(ListNode listNode){
         Stack<Integer> stack = new Stack<>();
