5fdfd10990c57b5a3de3a726072f18cf5c554a4b
binboy <dream_wt_possible@163.com>
2020-07-26 17:45:59 +0800

    commit "modify files"

--- src/jianzhiOffer/_19_正则表达式匹配.java
+++ src/jianzhiOffer/_19_正则表达式匹配.java
@@ -82,17 +82,21 @@ public class _19_正则表达式匹配 {
     public boolean isMatch_2(String s, String p){
         int ls = s.length();
         int lp = p.length();
+        //dp[i][j]代表字符串s中前i个字符和p中前j个字符是否匹配
+        //s的第i个字符记为s[m]==s[i-1]；p的第j个字符记为p[n] == p[j-1]
         boolean [][] dp = new boolean[ls + 1][lp + 1];
         dp[0][0] = true;
+        //为了保证空字符串和表达式匹配："",".*";或"","a*"这样只能匹配前一个字符0次，
         for(int j = 2; j <= lp; j++){
+            //初始化第一行，p的第j个字符记为'*'且dp[0][j-2]为true
             dp[0][j] = dp[0][j-2]&&p.charAt(j-1)=='*';
         }
         for(int i = 1; i <= ls; i++){
             for(int j = 1; j <= lp; j++){
                 int m = i -1, n = j - 1;
                 if(p.charAt(n) == '*'){
-                    dp[i][j] = dp[i][j-2] || dp[i-1][j] &&
-                            (s.charAt(m) == p.charAt(n-1) || p.charAt(n - 1) == '.');
+                    if((s.charAt(m) == p.charAt(n-1) || p.charAt(n - 1) == '.'))
+                        dp[i][j] = dp[i][j-2] || dp[i-1][j];
                 }
                 else if(s.charAt(m) == p.charAt(n) || p.charAt(n) == '.')
                     dp[i][j] = dp[i-1][j-1];
