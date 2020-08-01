529aa25535212833253a0523d0610bd0d53724dd
BINPC <dream_wt_possible@163.com>
2020-08-01 20:30:35 +0800

    commit "modify files"

--- src/written_examination/yuanFuDao/Main.java
+++ src/written_examination/yuanFuDao/Main.java
@@ -1,11 +1,51 @@
 package written_examination.yuanFuDao;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println("");
+        int n = sc.nextInt();
+        int [][] gap = new int[n][2];
+        int[] map = new int[n*2];
+        int [] hash = new int[n*2];
+        for(int i = 0; i < n; i++){
+            gap[i][0] = sc.nextInt();
+            gap[i][1] = sc.nextInt();
+            map[i*2] = gap[i][0];
+            map[i*2+1] = gap[i][1];
+        }
+        Arrays.sort(map);
+        int index = 1;
+        hash[0] = map[0];
+        for(int i = 1; i < n * 2; i++){
+            if(map[i] != map[i-1]){
+                hash[index++] = map[i];
+            }
+        }
+        int [] height = new int[index];
+        for(int i = 0; i < n; i++){
+            int s = find(hash,gap[i][0],index);
+            int b = find(hash,gap[i][1],index);
+            for(int j = s; j < b; j++){
+                height[j] += 1;
+            }
+        }
+        Arrays.sort(height);
+        System.out.println(height[index - 1]);
+    }
+    public static int find(int [] x, int target, int bund){
+        int left = 0;
+        int right = bund - 1;
+        while(left <= right){
+            int mid = left + (right - left) /2;
+            if(x[mid] == target)
+                return mid;
+            if (x[mid] < target)
+                left = mid + 1;
+            else
+                right = mid -1;
+        }
+        return -1;
     }
 }
--- src/written_examination/yuanFuDao/Main2.java
+++ src/written_examination/yuanFuDao/Main2.java
@@ -1,11 +1,44 @@
 package written_examination.yuanFuDao;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main2 {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println("");
+        int mod = 1000000003;
+        int N = sc.nextInt();
+        List<Integer> [] g = new ArrayList[N];
+        List<Integer> [] v = new ArrayList[N];
+        for(int i = 0; i < N; i++){
+            g[i] = new ArrayList<>();
+            v[i] = new ArrayList<>();
+        }
+        for(int i = 0; i < N; i++){
+            sc.nextLine();
+            int a, b;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            v[i].add(a);
+            if(b != 0) b = b - 2;
+            g[b].add(i);
+        }
+        int ans = 0;
+        for(int i = 0; i < g.length;i++){
+            int size = g[i].size();
+            int temp = 0;
+            if(size == 0)
+                continue;
+            for(int j = 0; j < size; j++){
+                int index = g[i].get(j);
+                if(v[index].get(0) < 0 && g[index].size() == 0){
+                    continue;
+                }
+                temp = temp + v[index].get(0) % mod;
+            }
+            ans = Math.max(ans, temp);
+        }
+        System.out.println(ans);
     }
 }
