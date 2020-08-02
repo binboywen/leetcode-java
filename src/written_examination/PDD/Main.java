fd7f12b1b7759e45f6dee89f39ea98069474540a
binboy <dream_wt_possible@163.com>
2020-08-02 18:45:56 +0800

    commit "modify files"

--- src/written_examination/PDD/Main.java
+++ src/written_examination/PDD/Main.java
@@ -1,4 +1,11 @@
 package written_examination.PDD;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" ");
+    }
 }
